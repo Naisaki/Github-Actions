4ea1ef8b86fcf1df91849819ea61eefbe468f005
ArianJC2002 <arianconqui@gmail.com>
2025-07-08 08:23:31 -0500

    c4 v6

--- Excercises/c4/statistic.js
+++ Excercises/c4/statistic.js
@@ -1,4 +1,11 @@
 function promedio(arr) {
+    if (!Array.isArray(arr) || arr.length === 0) {
+        throw new Error("Input must be a non-empty array");
+    }
+    
+    if (!arr.every(num => typeof num === 'number')) {
+        throw new Error("Todos los elementos del arreglo deben ser números");
+    }
     return arr.reduce((a, b) => a + b, 0) / arr.length;
 }
 
