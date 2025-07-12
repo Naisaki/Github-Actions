8ab156629b19dd67407d1b29254b24af8eb5fbf1
ArianJC2002 <arianconqui@gmail.com>
2025-07-12 09:57:14 -0500

    c6 v6

--- Exercises/c6/deploy.sh
+++ Exercises/c6/deploy.sh
@@ -1,7 +1,15 @@
+BRANCH=$1
+
+if [ "$BRANCH" == "main" ]; then
+  DEPLOY_PATH="/home/azureuser/Github-Actions"
+elif [ "$BRANCH" == "stage" ]; then
+  DEPLOY_PATH="/home/azureuser/stage"
+fi
+
 ssh -o StrictHostKeyChecking=no azureuser@20.55.25.47 <<HTML
 
-    cd /home/azureuser/Github-Actions
-    
-    git pull --rebase origin main
+    cd $DEPLOY_PATH
+
+    git pull --rebase origin $BRANCH
 
 HTML
